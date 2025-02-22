e113c56e37cdd9648e4491ccd291e2550117f92e
Tyler Kilburn <tylerkilburn@carfax.com>
2025-02-22 15:03:08 -0500

    f

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { Geist, Geist_Mono } from "next/font/google";
-import styles from "foobar/styles/Home.module.css";
+import styles from "../styles/Home.module.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
