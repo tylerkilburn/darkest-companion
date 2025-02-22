edc3c74ae59d1531332eb5bc6a195d606a6ddd28
Tyler Kilburn <tylerkilburn@carfax.com>
2025-02-22 18:33:12 -0500

    working through dungeon provisions

--- src/config.ts
+++ src/config.ts
@@ -30,11 +30,25 @@ const IMAGE = {
 
 const DUNGEON = {
   ANY: "any" as "any",
-  RUINS: "ruins" as "ruins",
+  COURTYARD: "Courtyard" as "Courtyard",
+  COVE: "Cove" as "Cove",
+  DARKEST_DUNGEON: "Darkest Dungeon" as "Darkest Dungeon",
+  FARMSTEAD: "Farmstead" as "Farmstead",
+  RUINS: "Ruins" as "Ruins",
+  WARRENS: "Warrens" as "Warrens",
+  WEALD: "Weald" as "Weald",
 }
 
 type IDungeonType = ValueOf<typeof DUNGEON>
 type ImageType = ValueOf<typeof IMAGE>
+
+type LengthType = "short" | "medium" | "long"
+
+const LENGTH = {
+  SHORT: "short" as "short",
+  MEDIUM: "medium" as "medium",
+  LONG: "long" as "long",
+}
 
 interface ICurio {
   dungeon: IDungeonType
@@ -63,18 +77,274 @@ interface IProvision {
   name: string
   price: number
   description: string
-  effort: string
+  effect: string
   stack: number
   image: ImageType
 }
 
+type ProvisionKeyType =
+  | "ANTIVENOM"
+  | "BANDAGE"
+  | "DOG_TREATS"
+  | "FIREWOOD"
+  | "FOOD"
+  | "LAUDANUM"
+  | "HOLY_WATER"
+  | "MEDICINAL_HERBS"
+  | "SHOVEL"
+  | "SKELETON_KEY"
+  | "TORCH"
+
 interface IConfig {
   curios: ICurio[]
+  provision: Record<ProvisionKeyType, IProvision>
 }
 
-const PROVISION = {
-  
+const PROVISION: Record<ProvisionKeyType, IProvision> = {
+  ANTIVENOM: {
+    name: "antivenom",
+    price: 150,
+    description: "Use to counter Blights, Poisons, and Toxins.",
+    effect: "Cures Blight",
+    stack: 6,
+    image: IMAGE.ANTIVENOM,
+  },
+  BANDAGE: {
+    name: "bandage",
+    price: 150,
+    description: "Use to staunch the flow of Bleeding.",
+    effect: "Cures Bleed",
+    stack: 6,
+    image: IMAGE.BANDAGE,
+  },
+  DOG_TREATS: {
+    name: "Dog Treats",
+    price: 0,
+    description:
+      "Automatically placed in the inventory when embarking with a Houndmaster. Can be consumed in battle for: +50% DMG +15 ACC The buff lasts for 3 rounds. Only usable by a Houndmaster.",
+    effect: "Buff",
+    stack: 2,
+    image: IMAGE.DOG_TREATS,
+  },
+  FIREWOOD: {
+    name: "Firewood",
+    price: 0,
+    description:
+      "Use to Camp. Cannot be bought but is instead given to the party based on the size of the dungeon that is explored: Short: 0 Medium: 1 Long: 2",
+    effect: "Begin Camp",
+    stack: 1,
+    image: IMAGE.FIREWOOD,
+  },
+  FOOD: {
+    name: "Food",
+    price: 75,
+    description: "Food is consumed to restore health and stave off hunger.",
+    effect: "Small Heal",
+    stack: 12,
+    image: IMAGE.FOOD,
+  },
+  LAUDANUM: {
+    name: "Laudanum",
+    price: 100,
+    description:
+      "A soothing tincture to inure one's mind against the horrors of the dark.",
+    effect: "Cures Horror ",
+    stack: 6,
+    image: IMAGE.LAUDANUM,
+  },
+  HOLY_WATER: {
+    name: "Holy Water",
+    price: 150,
+    description: "Use to purge evil and restore purity.",
+    effect: "Resists Debuff",
+    stack: 6,
+    image: IMAGE.HOLY_WATER,
+  },
+  MEDICINAL_HERBS: {
+    name: "Medicinal Herbs",
+    price: 200,
+    description: "Use to cure Diseases and Maladies.",
+    effect: "Cures Debuff",
+    stack: 6,
+    image: IMAGE.MEDICINAL_HERBS,
+  },
+  SHOVEL: {
+    name: "Shovel",
+    price: 250,
+    description: "Removes any obstacle out of the way.",
+    effect: "Removes Obstacles",
+    stack: 4,
+    image: IMAGE.SHOVEL,
+  },
+  SKELETON_KEY: {
+    name: "Skeleton Key",
+    price: 200,
+    description: "Use to unlock Strongboxes and Doors.",
+    effect: "Curios Only",
+    stack: 4,
+    image: IMAGE.SKELETON_KEY,
+  },
+  TORCH: {
+    name: "Torch",
+    price: 75,
+    description:
+      "Increases the Light level. In the Courtyard, it bestows the party with a +3 ACC buff for 1 battle.",
+    effect: "Light +25",
+    stack: 8,
+    image: IMAGE.TORCH,
+  },
 }
+
+interface IDungeonProvision {
+  dungeon: string
+  length: string
+  provisions: {
+    provision: IProvision
+    min: number
+    max: number
+  }[]
+}
+
+const DUNGEON_PROVISIONS: IDungeonProvision[] = [
+  {
+    dungeon: DUNGEON.RUINS,
+    length: LENGTH.SHORT,
+    provisions: [
+      {
+        provision: PROVISION.FOOD,
+        min: 8,
+        max: 12,
+      },
+      {
+        provision: PROVISION.SHOVEL,
+        min: 2,
+        max: 2,
+      },
+      {
+        provision: PROVISION.ANTIVENOM,
+        min: 0,
+        max: 1,
+      },
+      {
+        provision: PROVISION.BANDAGE,
+        min: 0,
+        max: 1,
+      },
+      {
+        provision: PROVISION.MEDICINAL_HERBS,
+        min: 1,
+        max: 2,
+      },
+      {
+        provision: PROVISION.SKELETON_KEY,
+        min: 2,
+        max: 3,
+      },
+      {
+        provision: PROVISION.HOLY_WATER,
+        min: 2,
+        max: 3,
+      },
+      {
+        provision: PROVISION.TORCH,
+        min: 8,
+        max: 10,
+      },
+    ],
+  },
+  {
+    dungeon: DUNGEON.RUINS,
+    length: LENGTH.MEDIUM,
+    provisions: [
+      {
+        provision: PROVISION.FOOD,
+        min: 16,
+        max: 20,
+      },
+      {
+        provision: PROVISION.SHOVEL,
+        min: 3,
+        max: 3,
+      },
+      {
+        provision: PROVISION.ANTIVENOM,
+        min: 0,
+        max: 2,
+      },
+      {
+        provision: PROVISION.BANDAGE,
+        min: 0,
+        max: 2,
+      },
+      {
+        provision: PROVISION.MEDICINAL_HERBS,
+        min: 2,
+        max: 3,
+      },
+      {
+        provision: PROVISION.SKELETON_KEY,
+        min: 2,
+        max: 4,
+      },
+      {
+        provision: PROVISION.HOLY_WATER,
+        min: 3,
+        max: 4,
+      },
+      {
+        provision: PROVISION.TORCH,
+        min: 10,
+        max: 14,
+      },
+    ],
+  },
+  {
+    dungeon: DUNGEON.RUINS,
+    length: LENGTH.LONG,
+    provisions: [
+      {
+        provision: PROVISION.FOOD,
+        min: 24,
+        max: 28,
+      },
+      {
+        provision: PROVISION.SHOVEL,
+        min: 3,
+        max: 4,
+      },
+      {
+        provision: PROVISION.ANTIVENOM,
+        min: 0,
+        max: 3,
+      },
+      {
+        provision: PROVISION.BANDAGE,
+        min: 0,
+        max: 3,
+      },
+      {
+        provision: PROVISION.MEDICINAL_HERBS,
+        min: 3,
+        max: 4,
+      },
+      {
+        provision: PROVISION.SKELETON_KEY,
+        min: 3,
+        max: 5,
+      },
+      {
+        provision: PROVISION.HOLY_WATER,
+        min: 3,
+        max: 5,
+      },
+      {
+        provision: PROVISION.TORCH,
+        min: 14,
+        max: 16,
+      },
+    ],
+  },
+]
 
 export const CONFIG: IConfig = {
   curios: [
@@ -298,6 +568,7 @@ export const CONFIG: IConfig = {
       ],
     },
   ],
+  PROVISION,
 }
 
 const imageMap: Record<ImageType, string> = {
